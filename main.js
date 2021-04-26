bc020170b3bd0502f58a74b300f24827cca04b01
Zuz <zuza0355@stud.kea.dk>
2021-04-26 19:38:12 +0200

    landing page

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import "./sass/style.scss"
+import "./sass/style.scss";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  document.querySelector("#cta-join").addEventListener("click", openFirstFormList);
+}
+
+function openFirstFormList() {
+  document.querySelector("#sign-up-form").classList.remove("hidden");
+  document.querySelector("#cta-join").classList.add("hidden");
+  document.querySelector("#landing-page").classList.add("blurred");
+}
